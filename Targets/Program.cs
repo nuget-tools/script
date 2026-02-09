e95919ed5184cb09eb2cb3de1ba16cdbd446bdab
JavaCommons Technologies <javacommons@gmail.com>
2026-02-09 22:33:25 +0900

    .

--- Targets/Program.cs
+++ Targets/Program.cs
@@ -3,6 +3,7 @@ using static SimpleExec.Command;
 
 Target("build", () => RunAsync("dotnet", "build --configuration Release"));
 Target("test", dependsOn: ["build"], () => RunAsync("dotnet", "test --configuration Release --no-build"));
+Target("dummy", () => RunAsync("bash", "-c 'dummy.sh'"));
 Target("default", dependsOn: ["test"]);
 
 await RunTargetsAndExitAsync(args, ex => ex is SimpleExec.ExitCodeException);
